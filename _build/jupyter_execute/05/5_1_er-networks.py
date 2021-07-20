5dca7393174590c4d0ad2b83638236b792d3594e
Kiril Zelenkovski <zelenkastiot@gmail.com>
2021-07-20 02:20:56 +0200

    Add all chapters before modifying; herd-im explanation is blank

--- _build/jupyter_execute/05/5_1_er-networks.py
+++ _build/jupyter_execute/05/5_1_er-networks.py
@@ -1,11 +1,29 @@
 # Ширење на вируси на ER мрежи
 
-"""
+## Erdős-Renyi 
 
- Created on 20-Feb-21
- @author: Kiril Zelenkovski
+Да почнеме со една од наједноставните нецелосни топологии. Оваа топологија првично беше истражена во длабочина кон крајот на 1950-тите од страна на Paul Erdős и Alfred Renyi (*Paul Erdős and Alfred Renyi. On random graphs. Publicationes Mathematicæ, 6:290–297, 1959*) . Тие го нарекоа нивниот пристап „случајни графови“, но тоа е малку погрешно име: можеме да произведеме „случајни мрежи“ со која било топологија како што видовме погоре. За среќа, влијанието на Erdős и Renyi е толку длабоко што мрежите од овој тип сега се нарекуваат **Erdős-Renyi** или **ЕР мрежи**.
 
-"""
+````{margin} **Кафе во теореми?**
+Alfred Renyi, другата половина од партнерството, e забележан математичар чија многу значајна друга работа беше прилично засенета од неговата прекрасно продуктивна асоцијација со Erdős. Тој беше одговорен за дефиницијата на математичар како „машина за претворање на кафето во теореми“.
+````
+
+```{figure} images/erdos_renyi.png
+---
+width: 40%
+align: center
+name: image
+---
+Erdős и Rényi. Левата слика е превземена од Technion. Десната слика од Paul Halmos.
+```
+
+
+Paul Erdős е еден од најплодните математичари на 20-от век. Перипатичен генијалец, тој во суштина не поседуваше ништо и патуваше меѓу универзитетските математички училишта барајќи проблеми за работа. Тој е исто така многу „централен“ во мрежата на математички коавтори, каде што нечиј „Erdős број“ (“Erdős number”) го мери бројот на коавторства меѓу нив и Erdős. (Помал подобар.) 
+
+Идејата за мрежата ЕР е дека постои раб помеѓу кој било пар јазли со одредена фиксна веројатност, а постоењето на кој било раб е независно од постоењето на кој било друг.
+
+За едноставен опис на дистрибуцијата на степенот, мрежата на ER е исто така многу едноставна за градење. Се избира голем број на јазли $N$ и се гради комплетен граф $K_N$. Комплетниот граф има раб помеѓу секој пар на јазли поради тоа бираме параметар $p_{edge}$ или $\phi$ помеѓу 0 и 1. Сега, се пуштаме за низ сите рабови, и за секој раб бираме случаен број $s$ помеѓу 0 и 1. Ако $s \le p_{edge}$, тогаш го држиме го работ; ако $s> p_ {edge}$, отстрануваме. На овој начин завршиваме со мрежа во која има раб помеѓу секој пар на јазли со веројатност $p_{edge}$. Ако $p_{edge} = 1$ го имаме **комплетниот граф** (секогаш држиме раб); ако $p_ {edge} = 0$ го имаме **празниот график** (никогаш не држиме раб); а за вредностите помеѓу нив имаме повеќе или помалку ретка колекција на рабови.
+
 import networkx
 import matplotlib
 import matplotlib.pyplot as plt
@@ -18,27 +36,29 @@ seaborn.set_context("notebook", font_scale=.8)
 (fig, axs) = plt.subplots(3, 2, sharex=True, sharey=True,
                           figsize=(8, 12))
 
-# a small network to visualise
+# Мала мрежа, 20 јазли
 N = 20
 
-# draw the network for different values of phi
+# Исцртување на мрежата за различни вредности на phi
 for (phi, ax) in [(0.05, axs[0][0]),
                   (0.10, axs[0][1]),
                   (0.25, axs[1][0]),
                   (0.50, axs[1][1]),
                   (0.75, axs[2][0]),
                   (0.99, axs[2][1])]:
-    # create a random ER network
+    # креирање на ER мрежа
     G = networkx.erdos_renyi_graph(N, phi)
 
-    # draw the network in the sub-plot
-    networkx.draw_circular(G, ax=ax)
-    ax.set_title('$p_{edge} = ' + '{p}$'.format(p=phi))
+    networkx.draw_circular(G, ax=ax, node_color='green')
+    ax.set_title('$\phi = ' + '{p}$'.format(p=phi))
 
-# fine-tune the figure
-plt.suptitle('ER мрежи за различни вредности на $p_{edge}$ ' + '($N = {n})$'.format(n=N))
+plt.suptitle('ER мрежи за различни вредности на $\phi$ ' + '($N = {n})$'.format(n=N))
 plt.savefig('images/2_2_1_er-networks-p.png')
 plt.show()
+
+Горе лево каде што $\phi$ е мал, имаме многу малку рабови: 5% од можниот вкупен број, всушност. Како што напредуваме кон долниот десен агол, мрежата станува сè погуста, сè додека на 99% од можните рабови не се поврзани и се тешко да се забележи дека воопшто недостасуваат рабови.
+
+Што е $\langle k \rangle$ за оваа топологија? Одговорот се покажа како доста едноставен. ЕР-мрежата има $\langle k \rangle = \frac{\phi}{N}$. Покрај тоа, дистрибуцијата на степенот - бројот на јазли со повеќе или помалку рабови од просечната, е $нормална дистрибуција$, „bell curve“ што ја гледаме во многу феномени.
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.gca()
@@ -54,23 +74,22 @@ g = networkx.gnp_random_graph(N, phi)
 
 # draw the degree distribution
 ks = list(dict(networkx.degree(g)).values())
-ax.hist(ks, bins=max(ks))
-ax.set_title('Ненасочена ER мрежа; degree distribution ($N = {n}, \\langle k \\rangle = {k}'.format(n=N, k=kmean) + ', p_{edge} = ' + '{p}$)'.format(p=phi), y=1.05)
+ax.hist(ks, bins=max(ks), color="green")
+ax.set_title('Ненасочена ER мрежа; степенска дист. ($N = {n}, \\langle k \\rangle = {k}'.format(n=N, k=kmean) + ', \phi = ' + '{p}$)'.format(p=phi), y=1.05)
 ax.set_xlabel('$k$')
 ax.set_ylabel('$N_k$')
 
 plt.savefig('images/2_2_2_degree-distribution-er.png')
 plt.show()
 
-"""
+Значи, околу една третина од јазлите (околу 650) имаат степен околу просекот (40), при што бројот на јазли со повеќе (или помалку) соседи опаѓа од двете страни на средната вредност. Постојат многу малку јазли со степени во единечни фигури, и многу малку со степени двојно повисоки - и нема со навистина голем степен. Всушност, за секоја мрежа ќе има **минимален степен** $k_{min}$ и **максимален степен** $k_{max}$. Дистрибуцијата на степени на мрежата ЕР вели дека максималниот степен никогаш не е „премногу далеку“ од просечниот (mean).
 
- Created on 20-Feb-21
- @author: Kiril Zelenkovski
+После целата таа научна мрежа, конечно можеме да си поставиме прашање поврзано со болести: што се случува кога варираме $\langle k \rangle = {k}$? Веќе знаеме дека кога $\langle k \rangle = {k} = (N − 1)$ мрежата има целосно мешање - комплетен граф. Но, што се случува кога $\langle k \rangle$ станува сè помал - односно луѓето имаат се помалку контакти едни со други - за истата болест? Веројатно би очекувале дека помалку луѓе ќе се заразат во целина, што исто така сугерира дека болеста во одреден момент едноставно ќе изумре без воопшто да влијае на многу луѓе.
 
-"""
-import epydemic
-import networkx
-import matplotlib.pyplot as plt
+За таа цел се обидов да направам интересна визуелизација. 
+
+Ги земеме истите параметри за големината на мрежата и болеста што ги имавме порано. 
+
 
 N = 2000
 T = 5000
@@ -78,6 +97,34 @@ pInfected = 0.01
 pInfect = 0.0001
 pRemove = 0.001
 
+Но, наместо да градиме комплетен граф, ќе градиме мрежи за ER и ќе видиме што ќе се случи додека го менуваме бројот за дистрибуцијата на степени $\langle k \rangle$.
+
+# Python3 imports 
+from scipy.integrate import odeint
+import numpy as np
+import math
+import plotly.graph_objects as go
+import plotly.tools as tls
+from plotly.validators.scatter.marker import SymbolValidator
+from plotly.offline import plot, iplot, init_notebook_mode
+from IPython.core.display import display, HTML
+import epydemic
+import networkx
+import matplotlib.pyplot as plt
+
+
+init_notebook_mode(connected = True)
+config={'showLink': False, 'displayModeBar': False}
+
+raw_symbols = SymbolValidator().values
+namestems = []
+namevariants = []
+symbols = []
+for i in range(0,len(raw_symbols),3):
+    name = raw_symbols[i+2]
+    symbols.append(raw_symbols[i])
+    namestems.append(name.replace("-open", "").replace("-dot", ""))
+    namevariants.append(name[len(namestems[-1]):])
 
 
 class MonitoredSIR(epydemic.SIR, epydemic.Monitor):
@@ -125,7 +172,8 @@ def network_sir(T, g, pInfected, pInfect, pRemove):
 
 
 epidemics = {}
-for kmean in [10, 20, 30, 40, 50, 100]:
+data = []
+for kmean in range(0, 101, 1):
     # create an ER network
     phi = (kmean + 0.0) / N
     g = networkx.gnp_random_graph(N, phi)
@@ -133,31 +181,105 @@ for kmean in [10, 20, 30, 40, 50, 100]:
     # run the corresponding simulation
     epidemics[kmean] = network_sir(T, g, pInfected, pInfect, pRemove)
 
-(fig, axs) = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(12, 15))
-
-# draw sub-plots for the different values of kmean
-for (kmean, ax) in [(10, axs[0][0]),
-                    (20, axs[0][1]),
-                    (30, axs[1][0]),
-                    (40, axs[1][1]),
-                    (50, axs[2][0]),
-                    (100, axs[2][1])]:
     (sim_ts, sim_sss, sim_iss, sim_rss) = epidemics[kmean]
-    ax.plot(sim_ts, sim_sss, 'g.', label='подлежни')
-    ax.plot(sim_ts, sim_iss, 'r.', label='заразени')
-    ax.plot(sim_ts, sim_rss, 'b.', label='оздравени')
-    ax.set_title('$\\langle k \\rangle = {k}$'.format(k=kmean))
-    ax.set_xlim([0, T])
-    ax.set_ylim([0, N])
 
-# fine-tune the figure
-plt.suptitle('SIR преку ER мрежи ($N = {n}'.format(n=N) + ', p_{\\mathit{Infect}} = ' + '{b:.4f}'.format(
-    b=pInfect) + ', p_{remove} = ' + '{a}$)'.format(a=pRemove))
-for i in [0, 1, 2]:
-    axs[i][0].set_ylabel('N')
-for i in [0, 1]:
-    axs[2][i].set_xlabel('$t$')
-axs[0][0].legend(loc='center right')
 
-plt.savefig('images/2_3_ER-SIR.png')
-plt.show()
+    data_1 = go.Scatter(x = sim_ts, 
+                        y = sim_sss, 
+                        mode='markers',
+                        visible = False,
+                        marker_color="blue",
+                        marker_symbol=symbols[5],
+                        name = "Подлежни; <i>S(t)</i>",
+                        hovertemplate = '<i> %{y:.0f} </i> луѓе')
+
+
+    data_2 = go.Scatter(x = sim_ts, 
+                        y = sim_iss,
+                        mode='markers',
+                        visible = False,
+                        marker_color="red",
+                        marker_symbol=symbols[5],
+                        name = "Заразени; <i>I(t)</i>",
+                        hovertemplate = '<i> %{y:.0f} </i> луѓе')
+
+
+    data_3 = go.Scatter(x = sim_ts,
+                        y = sim_rss, 
+                        mode='markers',
+                        visible = False,
+                        marker_color="green",
+                        marker_symbol=symbols[5],
+                        name = "Оздравени; <i>R(t)</i>",
+                        hovertemplate = '<i> %{y:.0f} </i> луѓе')
+
+    data.append(data_1)
+    data.append(data_2)
+    data.append(data_3)
+
+# Број на траги
+data_len = len(data)
+
+# Направи ги трагите за 50-та вредност видливи
+data[150]['visible'] = True
+data[151]['visible'] = True
+data[152]['visible'] = True
+
+# Креирај slider
+steps = []
+for i in range(0, 101, 1):
+    step = dict(
+        method = 'restyle',  
+        args = ['visible', [False]*data_len],
+        label = str(i))
+    for j in range(0,3):
+        step['args'][1][i*3+j] = True 
+    
+    steps.append(step)
+
+sliders = [dict(
+    active = 50,
+    currentvalue = {'prefix':
+                    "⟨k⟩ = <b>"},
+    pad = {"t": 80, "b": 10},
+    steps = steps
+)]
+
+# Изглед на фигура
+layout = go.Layout(title = 'SIR over ER networks (N = {n}'.format(n=N) + 
+                   ', p<sub>infect</sub> = ' + '{b:.4f}'.format(b=pInfect) + ', p<sub>remove</sub> = ' + '{a})'.format(a=pRemove),
+                  title_x = 0.5, 
+                  xaxis_title='<i>t</i>',
+                  xaxis=dict(range=[0,5000], 
+                             mirror=False,
+                             ticks='outside',
+                             showline=True,
+                             tickvals = [0, 1000, 2000, 3000, 4000, 5000],
+                             linecolor='#000',
+                             tickfont = dict(size=11)),
+                  yaxis_title='<i>N</i>',
+                  yaxis=dict(range=[0,2000], 
+                             mirror=False,
+                             ticks='outside', 
+                             showline=True,
+                             showspikes = True,
+                             linecolor='#000',
+                             tickvals = [0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000], 
+                             tickfont = dict(size=11)),
+                  plot_bgcolor='#fff', 
+                  hovermode = 'x unified',
+                  width = 700, 
+                  height = 450,
+                  sliders = sliders,
+                  font = dict(size = 10),
+                  margin=go.layout.Margin(l=50,
+                                         r=50,
+                                         b=60,
+                                         t=35))
+
+# Прикажи фигура 
+fig5 = dict(data=data, layout=layout)
+plot(fig5, filename = 'fig5.html', config = config)
+display(HTML('fig5.html'))
+
+Доколку ги споредиме графиците со оние порано и ќе ги видите истите шеми: менувањето на $\langle k \rangle$ го има истиот ефект како и менувањето на $p_{infect}$ ($\beta$) во смисла на поместување на врвот на инфекциите.
---
+++ _build/jupyter_execute/06/6_3_distance.py
@@ -0,0 +1,344 @@
+# Физичко дистанцирање
+
+import networkx
+import epyc
+import epydemic
+import pandas
+import mpmath
+import numpy
+
+import matplotlib
+%matplotlib inline
+%config InlineBackend.figure_format = 'png'
+matplotlib.rcParams['figure.dpi'] = 150
+import matplotlib.pyplot as plt
+import seaborn
+matplotlib.style.use('seaborn')
+seaborn.set_context("notebook", font_scale=1.75)
+
+def distanced_graph(N, clusterSizeDistribution, contactDistribution, clusterContactDistribution):
+    # build the initial graph
+    g = networkx.Graph()
+    
+    # build the clusters, each being a complete
+    # graph K_s of size s, labelled uniquely 
+    # within the overall graph
+    rng = np.random.default_rng()
+    n = 0
+    cid = 1
+    clusters = []
+    while n < N:
+        # build the cluster's graph with a random size
+        s = clusterSizeDistribution()
+        K_s = networkx.complete_graph(s)
+        
+        # relabel the cluster graph so all nodes in the final
+        # social distence graph have unique integer labels
+        networkx.relabel_nodes(K_s, lambda l: n + l,
+                               copy=False)
+        
+        # add to the graph and list of clusters
+        # label edges with the cluster they belong to
+        # (numbered from 1) and the size of the cluster
+        g.add_nodes_from(K_s.nodes,
+                         cluster=cid, cluster_size=s)
+        g.add_edges_from(K_s.edges,
+                         cluster=cid, cluster_size=s)
+        clusters.append(K_s)
+        n += s
+        cid += 1
+        
+    # draw the number of contact nodes per cluster from
+    # from the distribution
+    contacts = []
+    for c in clusters:
+        s = c.order()
+        d = contactDistribution(s)
+        contacts.append(d)
+        
+    # decide on the arity of each contact node
+    stubs = []
+    for i in range(len(clusters)):
+        c = clusters[i]
+        # first i node labels (since all nodes are
+        # identical in K_s) 
+        ls = list(c.nodes())[:contacts[i]]   
+        for l in ls:
+            # draw the number of contacts for this node
+            e = clusterContactDistribution(n)
+            
+            # append e copies of the node label to
+            # the list of stubs
+            stubs.extend([l] * e)
+    
+    # if number of stubs isn't even, add one to a
+    # randomly-chosen node (don't favour
+    # already-high-degree nodes
+    if len(stubs) % 2 > 0:
+        us = list(set(stubs))                
+        j = rng.integers(len(us))
+        stubs.append(us[j])
+        
+    # shuffle the stubs until there are no edge
+    # pair with the same endpoints
+    rng.shuffle(stubs) # may leave loops
+    while True:
+        # look for pairtings of stubs and others
+        # within the same cluster, and break them
+        # randomly (without changing the degree
+        # distribution)
+        swaps = 0
+        for i in range(0, len(stubs), 2):
+            if g.nodes[stubs[i]]['cluster'] == g.nodes[stubs[i + 1]]['cluster']:
+                # self loop, swap with another
+                # randomly-chosen stub
+                j = rng.integers(len(stubs))
+                t = stubs[i + 1]
+                stubs[i + 1] = stubs[j]
+                stubs[j] = t
+                
+                swaps += 1
+        if swaps == 0:
+            # no swaps, we're finished
+            break
+
+    # connect the nodes by pulling pairs of stubs
+    # and creating an edge between them
+    for i in range(0, len(stubs), 2):
+        # label inter-bubble edges as cluster 0 of size 0
+        g.add_edge(stubs[i], stubs[i + 1],
+                   cluster=0, cluster_size=0)
+    
+    # return the graph and list of cluster sizes
+    return (g, list(map(lambda h: h.order(), clusters)))
+
+def averageFamily():
+    rng = np.random.default_rng()
+    return max(int(rng.normal(4.5, 2)), 1)
+
+def coupleOfContacts(s):
+    rng = np.random.default_rng()
+    return max(int(rng.normal(min(s / 2, 2), 1)), 1)
+
+def expInterBubble(n):
+    rng = np.random.default_rng()
+    return max(int(rng.exponential(10.0)), 1)
+
+N = 1000
+
+(g, clusters) = distanced_graph(N, averageFamily, coupleOfContacts, expInterBubble)
+
+print('Mean family size of {s:.2f} (range {minf}-{maxf})'.format(s=np.mean(clusters), minf=min(clusters), maxf=max(clusters)))
+print('Most connected individual has {k} contacts'.format(k=max(dict(g.degree()).values())))
+
+def draw_distanced(g, cmap=None,
+                   color='cluster_size',
+                   ax=None):
+    # fill in defaults
+    if cmap is None:
+        cmap = plt.get_cmap('viridis')
+    if ax is None:
+        ax = plt.gca()
+    
+    # work out the colours 
+    ncs = list(map(lambda n: g.nodes[n][color],
+                   g.nodes()))
+    ecs = list(map(lambda e: g.edges[e][color],
+                   g.edges()))
+               
+    # draw with spring layout, which seems to
+    # give good results
+    networkx.draw_spring(g,
+                         ax=ax,
+                         with_labels=False,
+                         node_size=30,
+                         node_color=ncs,
+                         edge_color=ecs,
+                         cmap=cmap,
+                         edge_cmap=cmap)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.gca()
+
+# draw network
+cmap = plt.get_cmap('magma')
+draw_distanced(g, cmap=cmap)
+
+# add key (see https://matplotlib.org/examples/api/colorbar_only.html)
+bounds = list(range(min(clusters), max(clusters)))
+norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+ax1 = fig.add_axes([0.95, 0.15, 0.05, 0.7])
+cb2 = matplotlib.colorbar.ColorbarBase(ax1, cmap=cmap,
+                                    norm=norm,
+                                    boundaries=bounds,
+                                    ticks=bounds,
+                                    spacing='proportional',
+                                    orientation='vertical')
+
+plt.savefig('physical-distancing.png')
+_ = plt.show()
+
+
+
+# from https://nbviewer.jupyter.org/gist/minrk/4470122
+def pxlocal(line, cell):
+    ip = get_ipython()
+    ip.run_cell_magic("px", line, cell)
+    ip.run_cell(cell)
+get_ipython().register_magic_function(pxlocal, "cell")
+
+class MonitoredSIR(epydemic.SIR, epydemic.Monitor):
+ 
+    def __init__(self):
+        super(MonitoredSIR, self).__init__()
+        
+    def build(self, params):
+        '''Build the observation process.
+        
+        :param params: the experimental parameters'''
+        super(MonitoredSIR, self).build(params)
+
+        # also monitor other compartments
+        self.trackNodesInCompartment(epydemic.SIR.SUSCEPTIBLE)
+        self.trackNodesInCompartment(epydemic.SIR.REMOVED)
+
+class PhyDistNetworkDynamics(epydemic.StochasticDynamics):
+    
+    # Experimental parameters
+    N = 'N'
+    BUBBLE_MEAN = 'bubbleMean'
+    BUBBLE_STDDEV = 'bubbleStddev'
+    INTERBUBBLE_EXP= 'interBubbleExp'
+    
+    def __init__(self, p):
+        super(PhyDistNetworkDynamics, self).__init__(p)
+        
+    def bubbleSize(self):
+        rng = np.random.default_rng()
+        return max(int(rng.normal(self._bubbleMean,
+                                  self._bubbleStddev)), 1)
+
+    def contacts(self, s):
+        rng = np.random.default_rng()
+        return max(int(rng.normal(min(s / 2, 2), 1)), 1)    
+
+    def interBubble(self):
+        rng = np.random.default_rng()
+        return max(int(rng.exponential(self._interBubbleExponent)), 1)
+        
+    def distanced(self, N):
+        g = networkx.Graph()
+    
+        rng = np.random.default_rng()
+        n = 0
+        cid = 1
+        clusters = []
+        while n < N:
+            s = self.bubbleSize()
+            K_s = networkx.complete_graph(s)
+            
+            networkx.relabel_nodes(K_s,
+                                   lambda l: n + l, copy=False)
+            g.add_nodes_from(K_s.nodes,
+                             cluster=cid, cluster_size=s)
+            g.add_edges_from(K_s.edges,
+                             cluster=cid, cluster_size=s)
+            clusters.append(K_s)
+            n += s
+            cid += 1
+        
+        contacts = []
+        for c in clusters:
+            s = c.order()
+            d = self.contacts(s)
+            contacts.append(d)
+            
+        stubs = []
+        for i in range(len(clusters)):
+            c = clusters[i]
+            ls = list(c.nodes())[:contacts[i]] 
+            for l in ls:
+                e = self.interBubble()
+                stubs.extend([l] * e)
+        if len(stubs) % 2 > 0:
+            us = list(set(stubs))
+            j = rng.integers(len(us))
+            stubs.append(us[j])
+        
+        rng.shuffle(stubs)
+        while True:
+            swaps = 0
+            for i in range(0, len(stubs), 2):
+                if g.nodes[stubs[i]]['cluster'] == g.nodes[stubs[i + 1]]['cluster']:
+                    j = rng.integers(len(stubs))
+                    t = stubs[i + 1]
+                    stubs[i + 1] = stubs[j]
+                    stubs[j] = t
+                    swaps += 1
+            if swaps == 0:
+                break
+
+        for i in range(0, len(stubs), 2):
+            g.add_edge(stubs[i], stubs[i + 1],
+                       cluster=0, cluster_size=0)
+
+        return g
+
+    def configure(self, params):
+        super(PhyDistNetworkDynamics, self).configure(params)
+        
+        N = params[self.N]
+        self._bubbleMean = params[self.BUBBLE_MEAN]
+        self._bubbleStddev = params[self.BUBBLE_STDDEV]
+        self._interBubbleExponent = params[self.INTERBUBBLE_EXP]
+        g = self.distanced(N)
+        self.setNetworkGenerator(g)
+
+# network size
+N = 10000
+
+# simulation time
+T = 5000
+
+# disease parameters
+pInfected = 0.01
+pInfect = 0.0003
+pRemove = 0.002
+
+# experimental parameters common to both experiments
+params = dict()
+params[PhyDistNetworkDynamics.N] = 10000
+params[PhyDistNetworkDynamics.BUBBLE_MEAN] = 4.5
+params[PhyDistNetworkDynamics.BUBBLE_STDDEV] = 2.0
+params[PhyDistNetworkDynamics.INTERBUBBLE_EXP] = 10.0
+params[epydemic.SIR.P_INFECTED] = pInfected
+params[epydemic.SIR.P_INFECT] = pInfect
+params[epydemic.SIR.P_REMOVE] = pRemove
+params[epydemic.Monitor.DELTA] = T / 50
+
+# create model and experiment over distance network
+m = MonitoredSIR()
+m.setMaximumTime(T)
+e = PhyDistNetworkDynamics(m)
+
+rc = e.set(params).run()
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.gca()
+
+timeseries = rc[epyc.Experiment.RESULTS][epydemic.Monitor.TIMESERIES]
+tss = timeseries[epydemic.Monitor.OBSERVATIONS]
+sss = timeseries[epydemic.SIR.SUSCEPTIBLE]
+iss = timeseries[epydemic.SIR.INFECTED]
+rss = timeseries[epydemic.SIR.REMOVED]
+ax.plot(tss, sss, 'r-', label='susceptible')
+ax.plot(tss, iss, 'g-', label='infected')
+ax.plot(tss, rss, 'k-', label='removed')
+ax.set_xlabel('$t$')
+ax.set_ylabel('population that is...')
+ax.legend(loc='center right')
+
+# fine-time the figure
+ax.set_title('SIR on a physically distant network ($p_{\mathit{infect}} = ' + '{p}$)'.format(p=pInfect), y=1.05)
+
+_ = plt.show()
